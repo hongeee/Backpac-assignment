8150d91c4546936c58c5303cc6cd609013d31e0c
87ishong <87ishong@naver.com>
2021-08-08 22:30:09 +0900

    Initial commit

---
+++ src/main/java/com/hongeee/backpac/BackpacAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.hongeee.backpac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackpacAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackpacAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hongeee/backpac/BackpacAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongeee.backpac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackpacAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
